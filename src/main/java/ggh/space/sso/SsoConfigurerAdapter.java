ab24ee1d946c4e18c7723e309e441340c1c3f2e8
Ggh <gonggaohong@ggh.space>
2018-12-04 11:41:27 +0800

    feat:添加入口类

---
+++ src/main/java/ggh/space/sso/SsoConfigurerAdapter.java
@@ -0,0 +1,15 @@
+package ggh.space.sso;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author by ggh on 18-12-4.
+ */
+public class SsoConfigurerAdapter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
